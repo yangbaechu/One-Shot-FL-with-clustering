b3adab0d1e23dcfa0b2fe70b63f85e706928a6dc
yangbaechu <abab_1212@naver.com>
2023-05-07 14:07:48 +0900

    Get Metrics of One-Shot Learning

--- helper.py
+++ helper.py
@@ -72,3 +72,5 @@ def display_train_stats(cfl_stats, eps_1, eps_2, communication_rounds):
     # plt.ylim(0, 2)
 
     plt.show()
+
+    print(f"communication cost: {cfl_stats.mean_norm[-1]}, accuracy: {acc_mean[-1]}")
